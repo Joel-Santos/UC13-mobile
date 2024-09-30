d298ab8bdec9a912ab2bcd7721b2829df22f95b7
Joel Santos <joeloliveira56@gmail.com>
2024-09-30 22:48:56 +0000

    .

--- aulaAppFinanca/src/routes/auth.routes.js
+++ aulaAppFinanca/src/routes/auth.routes.js
@@ -2,3 +2,24 @@ import React from "react";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SingnIn from "../pages/SignIn";
+import SingnUp  from "../pages/SingnUp";
+
+const AuthStack = createNativeStackNavigator();
+
+function AuthRoutes(){
+    return (
+        <AuthStack.Navigator>
+            <AuthStack.Screen
+                name="SignIn"
+                component = {SingnIn}
+            />
+            <AuthStack.Screen 
+                name="SignIn"
+                component = {SingnIn}
+            />
+
+        </AuthStack.Navigator>
+    )
+}
+
+export default AuthRoutes;
