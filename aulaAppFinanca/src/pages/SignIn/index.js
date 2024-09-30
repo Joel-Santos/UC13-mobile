912f83d398d38da140732884e05adffb48621abf
Joel Santos <joeloliveira56@gmail.com>
2024-09-30 22:26:24 +0000

    teste

---
+++ aulaAppFinanca/src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from  'react'
+import {View, Text} from 'react-native'
+
+export default function SingnUp(){
+
+    return (
+        <View>
+            <Text>Tela de cadastro</Text>
+        </View>
+    )
+
+}
